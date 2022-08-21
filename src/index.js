c9d5bb0dd47c75cd43e28bad347ddbf26dd5f7d8
DmitriyR90 <ryabokon.dn@gmail.com>
2022-08-21 22:41:04 +0300

    made hw

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './js/fetchCountries';
+import {
+  renderCountriesList,
+  redrerCountryFinded,
+  clearCountryList,
+  clearCountryFinded,
+  showLoader,
+  hideLoader,
+} from './js/rendersFunctions';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('[id="search-box"]');
+
+let name;
+
+input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(e) {
+  name = e.target.value.trim();
+
+  if (name.length === 0) {
+    clearCountryList();
+    clearCountryFinded();
+  } else {
+    clearCountryFinded();
+
+    showLoader();
+
+    fetchCountries(name)
+      .then(countries => {
+        if (countries.length > 10) {
+          clearCountryList();
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+          return;
+        }
+
+        if (countries.length > 1) {
+          renderCountriesList(countries);
+          return;
+        }
+
+        if (countries.length === 1) {
+          redrerCountryFinded(countries);
+          return;
+        }
+      })
+      .finally(() => {
+        hideLoader();
+      });
+  }
+}
+
+//111222333
